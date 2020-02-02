dec0765cf874b33857ebc6faee035b2e66f8256d
Karen Imperial <karen.c.imperial@gmail.com>
2020-02-02 01:50:47 +0000

    Added test

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var assert = require('assert'),
+    http = require('http');
+
+ describe('/', function () {
+   it('should return 200', function (done) {
+     http.get('http://localhost:3000', function (res) {
+      assert.equal(200, res.statusCode);
+      done();
+    });
+  });
+});
+
